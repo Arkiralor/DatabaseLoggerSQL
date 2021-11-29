22844b58651059492bc850938ae07f543b183382
Arkiralor <pritho11335@gmail.com>
2021-11-29 13:11:45 +0530

    Minor changes.

--- logger.py
+++ logger.py
@@ -254,9 +254,10 @@ if __name__ == "__main__":
     # e1 = EventLogger('new_new_table')
     # e1.push_to_table('New new second mail!', slack=True, email=True)
 
-    e2 = EventLogger('error_table')
-    try:
-        print(3/0)
-    except Exception as err:
-        e2.push_to_table(f'Error: {err}', slack=True,
-                         email=False, hw_monitor=True)
+    # e2 = EventLogger('error_table')
+    # try:
+    #     print(3/0)
+    # except Exception as err:
+    #     e2.push_to_table(f'Error: {err}', slack=True,
+    #                      email=False, hw_monitor=True)
+    pass
